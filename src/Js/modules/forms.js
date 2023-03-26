0b126582ca47f594d6613006830335b0db683ad4
Ba6aka <bam6ykba6aka@gmail.com>
2023-03-26 16:44:12 +0300

    Formate code

--- src/Js/modules/forms.js
+++ src/Js/modules/forms.js
@@ -1,29 +1,31 @@
 import { postData } from "../servises/servises";
-import createNewMessage from "./create-newmessage";
 
 function forms() {
-
     //!FORMS REQUEST
     const forms = document.querySelectorAll('form');
-
 
     forms.forEach(item => {
         bindPostData(item);
     });
-    
-
 
     function bindPostData(form) {
-
         form.addEventListener('submit', (e) => {
-
             const formData = new FormData(form);
+            const today = new Date();
+            const now = today.toLocaleString();
 
-            let today = new Date();
-            let now = today.toLocaleString();
+            let autorColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            let authorName = document.getElementById('author-name').value;
+            
             formData.append('dateTime', now);
 
+            if (!localStorage.getItem(authorName)) {
+                localStorage.setItem(authorName, autorColor);
+            }
+            
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
+            
+         
 
             postData('http://localhost:3000/message', json)
                 .then((data) => {
@@ -34,7 +36,7 @@ function forms() {
                 });
 
         });
-        createNewMessage();
+        
     }
 }
 
